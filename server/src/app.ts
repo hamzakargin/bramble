6c51c9213d77cdf7e10eb8ae1e8378c45897395d
hamzakargin <hamzakargin@outlook.com>
2026-01-24 21:06:12 +0400

    Add app configuration, Create database connection, Define several models, Implemented basic route, Add scripts for development and production

---
+++ server/src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import authRoutes from "./routes/authRoutes";
+import userRoutes from "./routes/userRoutes";
+import chatRoutes from "./routes/chatRoutes";
+import messageRoutes from "./routes/messageRoutes";
+
+const app = express();
+
+app.use(express.json());
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chats", chatRoutes);
+app.use("/api/messages", messageRoutes);
+
+export default app;
