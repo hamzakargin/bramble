44d0a83d80c151566cd97196ec09a0d3a88cebdb
hamzakargin <hamzakargin@outlook.com>
2026-01-26 12:47:51 +0400

    feat: add Dockerfile and serve frontend in prod

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,6 +5,7 @@ import chatRoutes from "./routes/chatRoutes";
 import messageRoutes from "./routes/messageRoutes";
 import { clerkMiddleware } from "@clerk/express";
 import { errorHandler } from "./middleware/errorHandler";
+import path from "path";
 
 const app = express();
 
@@ -22,5 +23,10 @@ app.use("/api/chats", chatRoutes);
 app.use("/api/messages", messageRoutes);
 
 app.use(errorHandler);
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../web/dist")));
+  app.get("/{*any}", (_, res) => {
+    res.sendFile(path.join(__dirname, "../../web/dist/index.html"));
+  });
+}
 export default app;
